d3cf9b19b51189e849392658cf402f4def086310
k.czapla <k.czapla68@gmail.com>
2016-03-06 13:21:10 +0100

    Added basic lisp parser to interpreter. Added mpc lib for parsing. Exclude headers' files in .gitignore file

--- interactive_prompt.c
+++ interactive_prompt.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc.h"
 
 
 #ifdef _WIN32
@@ -30,6 +31,22 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv)
 {
+  // Parsers
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Lispy = mpc_new("lispy");
+
+  // Define parser with language
+  mpca_lang(MPCA_LANG_DEFAULT,
+	    "\
+              number : /-?[0-9]+/; \
+              operator : '+' | '-' | '*' | '/' | '%' | \
+                         \"add\" | \"sub\" | \"mul\" | \"div\"; \
+              expr : <number> |  '(' <operator> <expr>+ ')'; \
+              lispy : /^/<operator> <expr>+/$/; \
+            ",
+	    Number, Operator, Expr, Lispy);
   /* Print Version and Exit Infromation */
   puts("Lisp Version 0.0.0.0.1");
   puts("Press Ctrl+c to exit\n");
@@ -38,13 +55,25 @@ int main(int argc, char** argv)
   {
     // Output our prompt
     char * input = readline("lispy> ");
-
     // Add input to history
     add_history(input);
-    printf("No u're a %s\n", input);
 
+    // Attempt to prase the user input
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r))
+    {
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    }
+    else
+    {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+	
     // Free input
     free(input);
   }
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   return 0;
 }
